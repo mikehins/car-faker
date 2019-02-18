2177c51d34a8ab436e2cafe09380b5947cb1c6f4
Mike Hins <me@mikehins.com>
2019-02-18 11:04:55 -0500

    update car return
    Add possibility to return an array of bran and model

--- src/carfaker/provider/Car.php
+++ src/carfaker/provider/Car.php
@@ -16,18 +16,18 @@ class Car extends Base
      * @example 'Mercedes s500'
      */
     public function car($array = false)
-	{
-		$brandNames = array_keys(static::$brandModelNames);
-		$brand = static::randomElement($brandNames);
-		
-		$model = static::randomElementByKey(static::$brandModelNames, $brand);
-		
-		if ($array) {
-			return [$brand, $model];
-		}
-		
-		return $brand . ' ' . $model;
-	}
+    {
+        $brandNames = array_keys(static::$brandModelNames);
+        $brand = static::randomElement($brandNames);
+
+        $model = static::randomElementByKey(static::$brandModelNames, $brand);
+
+        if ($array) {
+            return [$brand, $model];
+        }
+
+        return $brand . ' ' . $model;
+    }
 
     /**
      * @example 'Audi'
